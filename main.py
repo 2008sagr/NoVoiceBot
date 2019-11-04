ab6755a28923c1d09117056aee8e7c6d20098180
2008sagr <2008sagr@gmail.com>
2019-11-04 23:23:03 +0200

    bugfixes

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import IAM_KEY
 from Yandex_S2T import s2t
 
 
+def randint():
+    randint = random.randint(100000000, 900000000)
+    return (randint)
+
+
 
 def main():
     token = ('e454041c2d0bc35bd37e0e8437c83e42b25742216d9c57643e18c61974f29d725c3a7bd796b73f98acd56')
-    randint = random.randint(100000000, 900000000)
     vk_session = vk_api.VkApi(token=token)
     vk_session._auth_token()
     botlongpool = VkBotLongPoll(vk_session ,group_id=188220345)
@@ -19,9 +23,9 @@ def main():
         if event.type == VkBotEventType.MESSAGE_NEW:
             print(event.object)
             parsing_str = str(event.object.message)
-            bot_keyword = parsing_str.lower().find('пиздюк')
+            bot_keyword = parsing_str.lower().find('кай ')
             if bot_keyword>-1:
-                vk.messages.send(chat_id=event.chat_id,message='Чего?',random_id=randint)
+                vk.messages.send(chat_id=event.chat_id,message='Пошел нахуй этот двоичный пидор!',random_id=randint())
             voice_msg = parsing_str.find('audio_message')
             if voice_msg > -1:
                 print('Голосовое сообщение')
@@ -43,11 +47,7 @@ def main():
                 v2t = s2t('b1g0g1nrj67c0se5efad', IAM_KEY.key)
                 print(v2t)
 
-
-                vk.messages.delete(message_ids=msg_id, delete_for_all=1)  # осталось понять почему это не работает
-
-
-                vk.messages.send(chat_id=event.chat_id, message=v2t, random_id=randint)
+                vk.messages.send(chat_id=event.chat_id, message=v2t, random_id=randint())
 
 
 
